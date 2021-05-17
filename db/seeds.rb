fa2df855314ba68148456e757e7a2812681d0721
Alice Menzie <Alice.Menzie@gmail.com>
2021-05-17 10:56:50 +1000

    SEEDS: created seeds file, fixed errors in model validations and relationship

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,55 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Destroying old records'
+User.destroy_all
+
+puts 'Seeding started'
+puts 'Creating DB'
+
+user1 = User.create(username: 'test',
+     email:'test@test.com',
+    password:'password')
+
+4.times do
+        user1.listings.create(
+        name: 'test',
+        description:'test description',
+        price: rand(20... 2_000.0),
+        category: 'test',
+        status: 1
+        )
+      end
+
+
+user1.addresses.create(
+    street: 'fake',
+    number: 22,
+    postcode: 3000,
+    state: 'VIC'
+)
+
+user2 = User.create(username: 'user2',
+  email:'user2@test.com',
+ password:'password')
+
+4.times do
+     user1.listings.create(
+     name: 'test2',
+     description:'test description2',
+     price: rand(20... 2_000.0),
+     category: 'test',
+     status: 1
+     )
+   end
+
+
+user1.addresses.create(
+ street: 'fake2',
+ number: 22,
+ postcode: 3123,
+ state: 'VIC'
+)
+
+puts 'Seeding complete'

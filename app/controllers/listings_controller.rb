a1f8b7d10cf8cc0976d4fe174340e98d9ed931f0
Alice Menzie <Alice.Menzie@gmail.com>
2021-05-20 14:54:36 +1000

    added some authorisation to listings

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,62 +1,67 @@
 class ListingsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :edit]
-  before_action :set_listing, only: [:show, :edit, :update, :destroy,]
-  
+  before_action :authenticate_user!, only: %i[new edit]
+  before_action :set_listing, only: %i[show]
+  before_action :authorize_listing, only: %i[edit update destroy]
+
   def index
-    @listings = Listing.all.sample(6)
-    end
+    @listings = Listing.active.sample(6)
+  end
 
   def new
     @listing = Listing.new
   end
 
-  def edit
-
+  def create
+    @listing = current_user.listings.new(listing_params)
+    @listing.status = 1
+    if @listing.save
+      redirect_to @listing
+    else
+      render :new
+    end
   end
 
-  def destroy 
-    @listing.destroy
-    flash[:alert] = 'deleted listing'
-    redirect_to listings_path
-  end 
+  def edit; end
 
+  
 
-
-  def update 
+  def update
     if @listing.update(listing_params)
       redirect_to @listing
-    else 
+    else
       render :edit
     end
-  end 
+  end
 
   def show
     @listing = Listing.find(params[:id])
-    end
+  end
 
+ 
 
-    def create 
-      @listing = current_user.listings.new(listing_params)
-      @listing.status = 1
-      if @listing.save 
-        redirect_to @listing
-      else 
-        render :new
-      end
-    end
-    
-    
-    private 
+  def destroy
+    @listing.destroy
+    flash[:alert] = 'Deleted Listing'
+    redirect_to listings_path
+  end
+
+  private
 
   def listing_params
     params.require(:listing).permit(:name, :description, :price, :category, :images)
   end
 
+  def authorize_listing
+    @listing = current_user.listings.find_by_id(params[:id])
+    return if @listing
+
+    flash[:alert] = 'You do not have permissions to access this page'
+    redirect_to listings_path
+  end
+
+
+
   def set_listing
     @listing = Listing.find(params[:id])
   end
-
-  
 end
-
-
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -4,4 +4,5 @@ class Listing < ApplicationRecord
   validates :price, numericality: {less_than_or_equal_to: 5_000}
   validates :description, length: {in: 2..500}
   has_many_attached :images
+  scope :active, -> { where(status: 1)}
 end
